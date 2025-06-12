3e09f953b7a641ce1420a7a2367b3c449b18b12a
Цветелин Маринов <Cvetelinmarinov566@gmail.com>
2025-06-12 22:27:31 +0300

    Add project files.

---
+++ 01.FirstProblem/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+using System.Reflection.Metadata;
+using System.Runtime.CompilerServices;
+
+/**
+ * 01.FirstProblem
+ */
+namespace _01.FirstProblem;
+
+class Program
+{
+    static void Main()
+    {
+        List<double> numbers = (Console.ReadLine() ?? "666 6 666")
+            .Split()
+            .Select(double.Parse)
+            .ToList();
+
+        for (int i = 0; i < numbers.Count - 1; ++i)
+        {
+            if (numbers[i] == numbers[i + 1])
+            {
+                numbers[i] += numbers[i + 1];
+                numbers.RemoveAt(i + 1);
+                i = -1;
+            }
+        }
+
+        Console.WriteLine(String.Join(' ', numbers));
+    }
+}
---
+++ 02.GaussTrick/Program.cs
@@ -0,0 +1,24 @@
+﻿/**
+ * 02.GaussTrick
+ */
+
+namespace _02.GaussTrick;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> numbers = (Console.ReadLine() ?? "6 6 6")
+            .Split()
+            .Select(int.Parse)
+            .ToList();
+
+        for (int i = 0; i <= numbers.Count / 2; i++)
+        {
+            numbers[i] += numbers[numbers.Count - 1];
+            numbers.RemoveAt(numbers.Count - 1);
+        }
+
+        Console.WriteLine(String.Join(' ', numbers));
+    }
+}
