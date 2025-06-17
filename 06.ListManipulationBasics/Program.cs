c13d7d39789447fcdaa98735c0a8eaaed4b79d72
Цветелин Маринов <Cvetelinmarinov566@gmail.com>
2025-06-17 21:57:02 +0300

    Задача 6 и проект за чернова

---
+++ 06.ListManipulationBasics/Program.cs
@@ -0,0 +1,64 @@
+﻿/*
+ * 06.ListManipulationBasics
+ */
+
+using System.Collections.Generic;
+
+namespace _06.ListManipulationBasics;
+
+class Program
+{
+    static void Main()
+    {
+        //4 19 2 53 6 43
+        //Add 3
+        //Remove 2
+        //RemoveAt 1
+        //Insert 8 3
+        //end
+
+        List<int> numbers = (Console.ReadLine() ?? "6 6 6 6")
+            .Split()
+            .Select(int.Parse)
+            .ToList();
+
+        string[] commandComponents = (Console.ReadLine() ?? "Add 666")
+            .Split();
+
+        while (commandComponents[0] != "end")
+        {
+            string cmd = commandComponents[0];
+            int parameter = int.Parse(commandComponents[1]);
+            int index = default;
+
+            if (commandComponents.Length == 3)
+            {
+                index = int.Parse(commandComponents[2]);
+            }
+
+            switch (cmd)
+            {
+                case "Add":
+                    numbers.Add(parameter);
+                    break;
+                case "Remove":
+                    numbers.Remove(parameter);
+                    break;
+                case "RemoveAt":
+                    numbers.RemoveAt(parameter);
+                    break;
+                case "Insert":
+                    numbers.Insert(index, parameter);
+                    break;
+                default:
+                    Console.WriteLine("Invalid command!");
+                break;
+            }
+
+            commandComponents = (Console.ReadLine() ?? "Add 1")
+            .Split();
+        }
+
+        Console.WriteLine(String.Join(' ', numbers));
+    }
+}
